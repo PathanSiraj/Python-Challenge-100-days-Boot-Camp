ec2f4911c91c3b2a5a6ecfe9438117eb43b2b471
PathanSiraj <pathansiraj11092006@gmail.com>
2025-02-21 13:40:43 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,5 @@
+print('welcome to the band new generator project!')
+name=input('what is your native place or city you grow up?')
+pname=input('what is your pet name?')
+print('your band name could be='+name+' '+pname)
+# (, and + both signs are used to join two senteces words are letters)
